ec0494543d1cdbf5867cb6ac77e1e4336f88e3d0
Francis L <francisl@FLYmacpro.local>
2021-07-31 20:16:53 -0400

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-AIzaSyBUFZv20HWnM8sS9P0LFAxwM5JTf2m2rE0
+function init() {
+  new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 59.325, lng: 18.069 },
+    zoom: 15
+  });
+}
+
+const marker = new google.maps.Marker({ map, position: initialPosition });
+
+  // Get user's location
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      position => console.log(`Lat: ${position.coords.latitude} Lng: ${position.coords.longitude}`),
+      err => alert(`Error (${err.code}): ${err.message}`)
+    );
+  } else {
+    alert('Geolocation is not supported by your browser.');
+  }
