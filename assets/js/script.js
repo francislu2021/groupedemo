dfc86a8a10cee579e86eb961b8d4e7cee92dd9a5
Francis L <francisl@FLYmacpro.local>
2021-07-31 22:28:13 -0400

    map5

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,44 +33,84 @@ function showError(error) {
 
 
 //google map display
-function init() {
-  const initialPosition = { lat: 59.325, lng: 18.069 };
-
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: initialPosition,
+/**
+* Create google maps Map instance.
+ * @param {number} lat
+ * @param {number} lng
+ * @return {Object}
+ */
+const createMap = ({ lat, lng }) => {
+  return new google.maps.Map(document.getElementById('map'), {
+    center: { lat, lng },
     zoom: 15
   });
+};
 
-  navigator.geolocation.getCurrentPosition(
-    // On success
-    position => console.log(`Lat: ${position.coords.latitude} Lng: ${position.coords.longitude}`),
-    // On error
-    err => alert(`Error (${err.code}): ${err.message}`)
-  );
+/**
+ * Create google maps Marker instance.
+ * @param {Object} map
+ * @param {Object} position
+ * @return {Object}
+ */
+const createMarker = ({ map, position }) => {
+  return new google.maps.Marker({ map, position });
+};
 
-  const marker = new google.maps.Marker({ map, position: initialPosition });
-
-  // Get user's location
-  if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(`Lat: ${position.coords.latitude} Lng: ${position.coords.longitude}`);
-
-        // Set marker's position.
-        marker.setPosition({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        });
-
-        // Center map to user's position.
-        map.panTo({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        });
-      },
-      err => alert(`Error (${err.code}): ${getPositionErrorMessage(err.code)}`)
-    );
-  } else {
-    alert('Geolocation is not supported by your browser.');
+/**
+ * Track the user location.
+ * @param {Object} onSuccess
+ * @param {Object} [onError]
+ * @return {number}
+ */
+const trackLocation = ({ onSuccess, onError = () => { } }) => {
+  if ('geolocation' in navigator === false) {
+    return onError(new Error('Geolocation is not supported by your browser.'));
   }
+
+  return navigator.geolocation.watchPosition(onSuccess, onError, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  });
+};
+
+/**
+ * Get position error message from the given error code.
+ * @param {number} code
+ * @return {String}
+ */
+const getPositionErrorMessage = code => {
+  switch (code) {
+    case 1:
+      return 'Permission denied.';
+    case 2:
+      return 'Position unavailable.';
+    case 3:
+      return 'Timeout reached.';
+  }
+}
+
+/**
+ * Initialize the application.
+ * Automatically called by the google maps API once it's loaded.
+*/
+function init() {
+  const initialPosition = { lat: 59.32, lng: 17.84 };
+  const map = createMap(initialPosition);
+  const marker = createMarker({ map, position: initialPosition });
+  const $info = document.getElementById('info');
+
+  let watchId = trackLocation({
+    onSuccess: ({ coords: { latitude: lat, longitude: lng } }) => {
+      marker.setPosition({ lat, lng });
+      map.panTo({ lat, lng });
+      $info.textContent = `Lat: ${lat.toFixed(5)} Lng: ${lng.toFixed(5)}`;
+      $info.classList.remove('error');
+    },
+    onError: err => {
+      console.log($info);
+      $info.textContent = `Error: ${err.message || getPositionErrorMessage(err.code)}`;
+      $info.classList.add('error');
+    }
+  });
 }
